b7f1f5dee5b6b0dd0e87bbf01309433c082eefcc
Ucis <98166442+Ucis-cmd@users.noreply.github.com>
2022-09-28 10:13:31 +0300

    Added winner history

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import random
 turns = ['rock', 'paper', 'scissors']
 human_turns = []
 computer_turns = []
+who_wins = []
 
 while(True):
     human_turn = input("Enter your turn, or type exit: ")
@@ -17,15 +18,21 @@ while(True):
     print(f'Human:{human_turn} vs. Computer:{computer_turn}')
     if human_turn == computer_turn:
         print('Draw!')
+        who_wins.append("Draw")
     elif human_turn == 'rock' and computer_turn == 'scissors':
         print('Human wins!')
+        who_wins.append("Human")
     elif human_turn == 'paper' and computer_turn == 'rock':
         print('Human wins!')
+        who_wins.append("Human")
     elif human_turn == 'scissors' and computer_turn == 'paper':
         print('Human wins!')
+        who_wins.append("Human")
     else:
         print('Computer wins!')
+        who_wins.append("Computer")
 
 print(f"played games: {len(human_turns)}")
 print(human_turns)
 print(computer_turns)
+print(who_wins)
